561d2ac3241d6fe4c02e0d562545906e009a55b0
Adrian Chipana C. <adrianchipana1@gmail.com>
2025-08-19 11:55:28 -0500

    Update site info

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,5 +1,5 @@
 export const personalInfo = {
-  name: "Lorem Ipsum",
+  name: "J. Adrian Chipana Cueva",
   location: "Dolor Sit Amet",
   email: "lorem@ipsum.com",
   github: "https://github.com/loremipsum",
