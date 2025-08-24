a8058e542f5b672912bf2d9a89844808fef2ea9c
Adrian Chipana C. <adrianchipana1@gmail.com>
2025-08-24 16:26:03 -0500

    Update Page Info

--- src/components/GlassHeader.tsx
+++ src/components/GlassHeader.tsx
@@ -6,7 +6,7 @@ import { motion, AnimatePresence } from "framer-motion";
 
 export default function GlassHeader() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+  const sections = ["experience", "skills", "education"]; 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
@@ -23,7 +23,7 @@ export default function GlassHeader() {
 
         {/* Desktop Navigation */}
         <nav className="hidden md:flex items-center space-x-6 text-sm font-medium">
-          {["experience", "skills", "projects", "awards", "education"].map(
+          {[sections].map(
             (item, index) => (
               <motion.a
                 key={item}
@@ -71,7 +71,7 @@ export default function GlassHeader() {
             transition={{ duration: 0.3 }}
           >
             <nav className="flex flex-col space-y-4 text-sm font-medium">
-              {["experience", "skills", "education"].map(
+              {[sections].map(
                 (item, index) => (
                   <motion.a
                     key={item}
